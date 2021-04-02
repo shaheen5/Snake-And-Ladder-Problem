fbcd8ab8a24e24485c742c22df388f7c93d73da5
unknown <miyashaheen1995@gmail.com>
2021-04-02 20:20:55 +0530

    Added : Function to get no. between [1-6] using random method

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,8 +7,11 @@ public class SnakeAndLadder {
 		int start=0;
 		int player_position=start;
 		System.out.println("\n******************Game Starts******************\n"+"Player position is "+player_position);
+		int move_positions=rollDie();
+		System.out.println("Number of positions to move :"+move_positions);
 			
 	}
-	
-	
+	public static int rollDie() {
+		return (int)(Math.random()*6)+1;
+	}	
 }
