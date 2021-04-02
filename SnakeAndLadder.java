afbd50cce0aab931639df4d519a435d759bfed6d
unknown <miyashaheen1995@gmail.com>
2021-04-02 20:08:18 +0530

    Added : Code to initialize player position to 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,14 @@
 package Snake_Ladder_Problem;
 
 public class SnakeAndLadder {
-
-	public static void main(String[] args) {
+	
+	public static void main(String args[]) {
 		System.out.println("Welcome to Snake and Ladder Simulation Program !!!");
+		int start=0;
+		int player_position=start;
+		System.out.println("\n******************Game Starts******************\n"+"Player position is "+player_position);
+			
 	}
-
+	
+	
 }
